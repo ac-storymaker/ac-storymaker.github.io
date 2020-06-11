04811c46d1dc52966341d15528ad008ef230c6a7
vanstorm9 <antlowhur@yahoo.com>
2020-06-10 21:53:45 -0400

    Adding new GIF

--- js/analytics.js
+++ js/analytics.js
@@ -3,5 +3,5 @@
 	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-	  ga('create', 'G-C3XZW1TFJ8', 'auto');
+	  ga('create', 'G-639VB7MVHP', 'auto');
 	  ga('send', 'pageview');
